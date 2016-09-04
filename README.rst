dc3f38d5629f28fcf2034dd1a18f2d046f247ca0
Alexis Bellido <alexis@ventanazul.com>
2016-09-04 16:08:09 +0000

    first import with pip support

---
+++ README.rst
@@ -0,0 +1,9 @@
+========================
+README
+========================
+
+Django application used by the zinibu project, based on django-zinibu-skeleton.
+
+The application provides:
+
+* The basics for creating a reusable Django application with setuptools that can be installed from the Python Package Index (PyPI) via pip.
---
+++ setup.py
@@ -0,0 +1,65 @@
+"""A setuptools based setup module for a Django application, based on django-zinibu-skeleton
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+from znbseo import get_version
+
+setup(
+    name='django-zinibu-seo',
+    version=get_version().replace(' ', '-'),
+    description='A seo Django application',
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    author='Alexis Bellido',
+    author_email='a@zinibu.com',
+    license='BSD, see LICENSE file',
+    url='https://zinibu.com',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Environment and framework details
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    zip_safe=False,
+)
