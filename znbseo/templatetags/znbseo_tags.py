f6fe6703846239b64fbc30a494fd78d25a093c1b
Alexis Bellido <alexis@ventanazul.com>
2016-09-13 01:32:02 +0000

    refactor get_absolute_url in znbmain

--- znbseo/templatetags/znbseo_tags.py
+++ znbseo/templatetags/znbseo_tags.py
@@ -1,8 +1,7 @@
 from django import template
-from django.conf import settings
-from django.contrib.sites.models import Site
 
 from znbmain.models import Article
+from znbmain.utils import get_absolute_url, is_production
 
 
 register = template.Library()
@@ -14,17 +13,17 @@ def robots_snippet(request, object=None, index=True):
     live articles.
     """
     meta_robots = "noindex, nofollow"
-    running_env = getattr(settings, 'ZNBSEO_IS_PRODUCTION', False)
-    if running_env and index:
+    if is_production and index:
         if (object and object.status == Article.LIVE_STATUS) or (request.path == '/'):
             meta_robots = "index, follow"
-    return {'meta_robots': meta_robots}
+    return {
+        'meta_robots': meta_robots
+    }
 
 @register.inclusion_tag('znbseo/canonical.html')
 def canonical(request, object=None):
-    if object:
-        url = object.get_absolute_url()
-    else:
+    if not object:
         url = request.path
-    url = 'http://%s%s' % (Site.objects.get_current().domain, url)
-    return {'url': url}
+    return {
+        'url': get_absolute_url(object, False)
+    }
